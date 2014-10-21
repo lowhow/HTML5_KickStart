cdd5af6b18387734269ae20fa9ef186e97937a7d
Low How <lowhow@gmail.com>
2014-10-21 18:44:42 +0800

    Ready to go

---
+++ assets/scripts/modernizr.js
@@ -0,0 +1,1 @@
+// @codekit-append "../bower_components/modernizr/modernizr.js";
---
+++ assets/scripts/vendor.js
@@ -0,0 +1,1 @@
+// @codekit-append "../bower_components/jquery/dist/jquery.min.js", "../bower_components/bootstrap/dist/js/bootstrap.min.js", "../bower_components/jQuery.mmenu/src/js/jquery.mmenu.min.all.js", "../bower_components/angularjs/angular.min.js";
